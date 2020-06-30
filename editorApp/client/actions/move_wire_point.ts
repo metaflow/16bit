d395a7345d24705f307698715bd8e226829053b1
Mikhail Goncharov <goncharov.mikhail@gmail.com>
2020-06-30 19:45:17 +0100

    Rework of move wire point action
    now each individual wire state is isolated in a separate object
    and action doesn't need as much bookkeeping. WIP as now action doesn't
    move wire or displays a preview but later this structure will allow
    to show live preview by constructing aux Wire instances and applying
    spec to them instead of modifying original instance. Modifying original
    wire has some issues, for example affected point might be gone in the
    middle of the move as it is redundant. Now original spec / wire is kept
    so we always keep original objects in address space.

--- editorApp/client/actions/move_wire_point.ts
+++ editorApp/client/actions/move_wire_point.ts
@@ -19,77 +19,77 @@ interface spec {
   points: string[];
   from: [number, number];
   to: [number, number];
-  wires: string[];
-  oldWireStates: Map<string, WireSpec>;
+  states: WireState[];
+}
+
+interface WireState {
+  address: string;
+  originalSpec: WireSpec;
+  affectedPointsIds: string[];
+};
+
+function moveSingleWire(x: number, y : number, s: WireState): WireSpec {
+return s.originalSpec;
 }
 
 export class MoveWirePointAction implements Action {
   actionType = "MoveWirePointAction";
-  oldWireStates = new Map<string, WireSpec>();
-  newWireStates = new Map<string, WireSpec>();
-  original: WirePointSpec[] = [];
-  wires: string[];
+  states: WireState[] = [];
+  affectedPointsAddresses: string[];
   from: [number, number];
   to: [number, number];
   constructor(points: WirePoint[], origin?: [number, number]) {
-    this.original = points.map(p => p.spec());
-    this.wires = Array.from(new Set<string>(points.map(p => p.parent()?.address()!)))
-    for (const w of this.wires) {
-      this.oldWireStates.set(w, (getByAddress(w) as Wire).spec());
+    this.affectedPointsAddresses = points.map(p => p.address());
+    const uniqAdresses = Array.from(new Set<string>(points.map(p => p.parent()?.address()!)));
+    for (const a of uniqAdresses) {
+      const w = getByAddress(a) as Wire;
+      const s: WireState = {
+        address: w.address(),
+        originalSpec: w.spec(),
+        affectedPointsIds: [],
+      };
+      for (const p of points) {
+        if (p.parent() == w) {
+          s.affectedPointsIds.push(p.id());
+        }
+      }
+      this.states.push(s);
     }
+    console.log('wire move states', this.states);
     if (origin === undefined) origin = getPhysicalCursorPosition();
     this.from = origin;
     this.to = origin;
-    // this.points = points;
   }
   serialize(): any {
     const z: spec = {
-      points: this.original.map(p => p.address || ''),
+      points: this.affectedPointsAddresses,
       from: this.from,
       to: this.to,
-      wires: this.wires,
-      oldWireStates: this.oldWireStates,
+      states: this.states,
     };
     return {
       'typeMarker': 'MoveWirePointAction',
       'spec': z,
     }
   }
-  updatePositions() {
+  apply(): void {
     const dx = this.to[0] - this.from[0];
     const dy = this.to[1] - this.from[1];
-    for (const s of this.original) {
-      let p = assertExists(getTypedByAddress(WirePoint, s.address));
-      p.x(s.x + dx);
-      p.y(s.y + dy);
-    }
-  }
-  apply(): void {
-    this.updatePositions();
-    for (const s of this.original) {
-      let p = assertExists(getTypedByAddress(WirePoint, s.address));
-      p._helper = false;
-      p.fixed = true;
-    }
-    for (const w of this.wires) {
-      (getByAddress(w) as Wire).updateLayout();
-    }
-    for (const w of this.wires) {
-      this.newWireStates.set(w, (getByAddress(w) as Wire).spec());
+    for (const w of this.states) {
+      (getByAddress(w.address) as Wire).spec(moveSingleWire(dx, dy, w));
     }
   }
   undo(): void {
-    this.oldWireStates.forEach((v, k) => {
-      let w = (getByAddress(k) as Wire);
-      w.spec(v);
-    });
+    for (const w of this.states) {
+      (getByAddress(w.address) as Wire).spec(w.originalSpec);
+    }
   }
   mousemove(event: Konva.KonvaEventObject<MouseEvent>): boolean {
     this.to = getPhysicalCursorPosition();
-    this.updatePositions();
-    for (const w of this.wires) {
-      (getByAddress(w) as Wire).updateLayout();
-    }
+// TODO: update aux wires
+    // for (const w of this.wires) {
+      // (getByAddress(w) as Wire).updateLayout();
+    // }
     return false;
   }
   mousedown(event: Konva.KonvaEventObject<MouseEvent>): boolean {
